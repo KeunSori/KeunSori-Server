a9ea4672577671670d98d189a8246b7774909eb5
kckc0608 <kckc0608@gmail.com>
2025-01-03 17:53:49 +0900

    feat: init project

---
+++ src/main/java/com/keunsori/keunsoriserver/KeunSoriServerApplication.java
@@ -0,0 +1,13 @@
+package com.keunsori.keunsoriserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeunSoriServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeunSoriServerApplication.class, args);
+    }
+
+}
