f0fe2c80169042a5c581f569962504f47ee2d96e
kckc0608 <kckc0608@gmail.com>
2025-01-13 13:57:57 +0900

    feat: reservation repository 작성

---
+++ src/main/java/com/keunsori/keunsoriserver/reservation/ReservationRepository.java
@@ -0,0 +1,7 @@
+package com.keunsori.keunsoriserver.reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
