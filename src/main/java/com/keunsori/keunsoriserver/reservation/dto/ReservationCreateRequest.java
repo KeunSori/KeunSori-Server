a2521c063b5e96cc0e6289683eca0e506c593819
kckc0608 <kckc0608@gmail.com>
2025-01-16 15:09:48 +0900

    feat: swagger 적용

--- src/main/java/com/keunsori/keunsoriserver/reservation/dto/ReservationCreateRequest.java
+++ src/main/java/com/keunsori/keunsoriserver/reservation/dto/ReservationCreateRequest.java
@@ -5,14 +5,18 @@ import com.keunsori.keunsoriserver.reservation.Reservation;
 import com.keunsori.keunsoriserver.reservation.vo.ReservationType;
 import com.keunsori.keunsoriserver.reservation.vo.Session;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.sql.Time;
 import java.util.Date;
 
 public record ReservationCreateRequest(
         ReservationType reservationType,
         Session reservationSession,
+        @Schema(example = "2025-01-01", type = "string")
         Date reservationDate,
+        @Schema(example = "21:00:00", type = "string")
         Time reservationStartTime,
+        @Schema(example = "21:00:00", type = "string")
         Time reservationEndTime
 ) {
     public Reservation toEntity(Member member) {
