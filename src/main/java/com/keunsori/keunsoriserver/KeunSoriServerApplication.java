ae5aad06dca93a96610097491e3b520f87a0b2dd
YuYeonho <127312366+yuuuyeonho@users.noreply.github.com>
2025-01-17 17:12:52 +0900

    feat: Base Entity 작성, Member 엔티티 approvalDate 추가 (#12)
    * feat: Base Entity 작성, Member 엔티티 approvalDate 추가
    
    * 수정사항 적용

--- src/main/java/com/keunsori/keunsoriserver/KeunSoriServerApplication.java
+++ src/main/java/com/keunsori/keunsoriserver/KeunSoriServerApplication.java
@@ -2,7 +2,9 @@ package com.keunsori.keunsoriserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class KeunSoriServerApplication {
 
---
+++ src/main/java/com/keunsori/keunsoriserver/domain/member/domain/vo/MemberStatus.java
@@ -0,0 +1,5 @@
+package com.keunsori.keunsoriserver.domain.member.domain.vo;
+
+public enum MemberStatus {
+    승인대기, 일반, 관리자
+}
