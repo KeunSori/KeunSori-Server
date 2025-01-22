a45dc3ea0703c4fe279d1c2875420c181c6d34ca
kckc0608 <kckc0608@gmail.com>
2025-01-23 01:33:30 +0900

    feat: 500 error 처리 & 로깅

--- src/main/java/com/keunsori/keunsoriserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/keunsori/keunsoriserver/global/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -41,5 +44,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleReservationException(ReservationException ex) {
         ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
         return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+        log.error(ex.getMessage());
+        return ResponseEntity.internalServerError().body(response);
     }
 }
