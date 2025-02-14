83b2e874a6115380ee8ee264d40bc57427f12fdd
Everdu <kckc0608@gmail.com>
2025-02-14 22:48:25 +0900

    chore: cors origin 세팅 (#61)
    * chore: cors origin 세팅
    
    * chore: 불필요 설정 파일 삭제
    
    * chore: 백엔드 개발서버 url 추가

--- src/main/java/com/keunsori/keunsoriserver/global/constant/EnvironmentConstant.java
+++ src/main/java/com/keunsori/keunsoriserver/global/constant/EnvironmentConstant.java
@@ -7,6 +7,7 @@ public class EnvironmentConstant {
     public static final String LOCAL_URL_3 = "http://localhost:8080";
 
     public static final String DEV_URL = "https://keun-develop.vercel.app";
+    public static final String DEV_URL2 = "https://keunsori-api.everdu.com";
 
     public static final String PROD_URL = "https://keun-sori-web.vercel.app";
 }
