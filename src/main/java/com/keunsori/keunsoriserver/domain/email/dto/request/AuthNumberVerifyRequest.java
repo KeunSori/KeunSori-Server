4388322cc0c9bb49449f18c47e5fe9dbe346afc8
kwon <kckc0608@naver.com>
2025-02-23 16:47:52 +0900

    feat: 이메일 형식 검증 및 에러메세지 추가

--- src/main/java/com/keunsori/keunsoriserver/domain/email/dto/request/AuthNumberVerifyRequest.java
+++ src/main/java/com/keunsori/keunsoriserver/domain/email/dto/request/AuthNumberVerifyRequest.java
@@ -1,8 +1,11 @@
 package com.keunsori.keunsoriserver.domain.email.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthNumberVerifyRequest(
-        @NotBlank String email,
-        @NotBlank String authNumber
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.") String email,
+        @NotBlank(message = "인증번호는 필수입니다.")
+        String authNumber
 ) {}
