78296709b7dbd75f6d286095316ca8f0b00e0264
kckc0608 <kckc0608@naver.com>
2025-03-24 23:52:53 +0900

    test: 비밀번호 초기화 테스트 작성

---
+++ src/test/java/com/keunsori/keunsoriserver/auth/service/AuthServiceTest.java
@@ -0,0 +1,100 @@
+package com.keunsori.keunsoriserver.auth.service;
+
+import com.keunsori.keunsoriserver.domain.auth.dto.request.PasswordInitializeRequest;
+import com.keunsori.keunsoriserver.domain.auth.service.AuthService;
+import com.keunsori.keunsoriserver.domain.member.domain.Member;
+import com.keunsori.keunsoriserver.domain.member.repository.MemberRepository;
+import com.keunsori.keunsoriserver.global.exception.MemberException;
+import com.keunsori.keunsoriserver.global.util.EmailUtil;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static com.keunsori.keunsoriserver.global.exception.ErrorMessage.MEMBER_NOT_EXISTS_WITH_STUDENT_ID;
+import static com.keunsori.keunsoriserver.global.exception.ErrorMessage.STUDENT_ID_DOES_NOT_MATCH_WITH_EMAIL;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private EmailUtil emailUtil;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Test
+    void 비밀번호_초기화_성공() {
+        // given
+        Member member = Member.builder()
+                .studentId("C011013")
+                .password("123456")
+                .email("test@gmail.com")
+                .build();
+
+        PasswordInitializeRequest request = new PasswordInitializeRequest(
+                "C011013",
+                "test@gmail.com"
+        );
+
+        given(memberRepository.findByStudentIdIgnoreCase("C011013")).willReturn(Optional.of(member));
+        given(passwordEncoder.encode(anyString())).willReturn("NEW_PASSWORD_HASH");
+
+        // when
+        authService.initializePassword(request);
+
+        // then
+        Assertions.assertThat(member.getPassword()).isEqualTo("NEW_PASSWORD_HASH");
+    }
+
+    @Test
+    void 비밀번호_초기화_실패_미존재_학번() {
+        // given
+        PasswordInitializeRequest request = new PasswordInitializeRequest(
+                "C011013",
+                "test@gmail.com"
+        );
+
+        given(memberRepository.findByStudentIdIgnoreCase("C011013")).willReturn(Optional.empty());
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> authService.initializePassword(request))
+                .isInstanceOf(MemberException.class)
+                .hasMessage(MEMBER_NOT_EXISTS_WITH_STUDENT_ID);
+    }
+
+    @Test
+    void 비밀번호_초기화_실패_학번_이메일_불일치() {
+        // given
+        Member member = Member.builder()
+                .studentId("C011013")
+                .password("123456")
+                .email("test@gmail.com")
+                .build();
+
+        PasswordInitializeRequest request = new PasswordInitializeRequest(
+                "C011013",
+                "test2@gmail.com"
+        );
+
+        given(memberRepository.findByStudentIdIgnoreCase("C011013")).willReturn(Optional.of(member));
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> authService.initializePassword(request))
+                .isInstanceOf(MemberException.class)
+                .hasMessage(STUDENT_ID_DOES_NOT_MATCH_WITH_EMAIL);
+    }
+}
