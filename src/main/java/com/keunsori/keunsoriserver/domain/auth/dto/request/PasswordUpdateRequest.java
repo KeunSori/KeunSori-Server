e2d4f059f9a7f36ae8bad6ea92bd49aa4e215a69
kckc0608 <kckc0608@naver.com>
2025-05-05 23:11:34 +0900

    refactor: 링크 기반 비밀번호 변경 요청시 요청 데이터 검증

--- src/main/java/com/keunsori/keunsoriserver/domain/auth/dto/request/PasswordUpdateRequest.java
+++ src/main/java/com/keunsori/keunsoriserver/domain/auth/dto/request/PasswordUpdateRequest.java
@@ -1,6 +1,14 @@
 package com.keunsori.keunsoriserver.domain.auth.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import static com.keunsori.keunsoriserver.global.constant.RequestFormatConstant.PASSWORD_REGEX;
+import static com.keunsori.keunsoriserver.global.exception.ErrorMessage.PASSWORD_INVALID_FORMAT;
+
 public record PasswordUpdateRequest(
+        @NotNull(message = "비밀번호 변경을 위한 토큰값은 필수입니다.")
         String token,
+        @Pattern(regexp = PASSWORD_REGEX, message = PASSWORD_INVALID_FORMAT)
         String newPassword
 ) {}
