19ac8432c66aef5755de834e69146e1fba7da58d
kckc0608 <kckc0608@naver.com>
2025-05-05 23:15:02 +0900

    refactor: 공통 검증 메세지 상수화

--- src/main/java/com/keunsori/keunsoriserver/domain/auth/dto/request/PasswordUpdateLinkSendRequest.java
+++ src/main/java/com/keunsori/keunsoriserver/domain/auth/dto/request/PasswordUpdateLinkSendRequest.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 
 import static com.keunsori.keunsoriserver.global.constant.RequestFormatConstant.STUDENT_ID_REGEX;
+import static com.keunsori.keunsoriserver.global.exception.ErrorMessage.EMAIL_INVALID_FORMAT;
+import static com.keunsori.keunsoriserver.global.exception.ErrorMessage.STUDENT_ID_INVALID_FORMAT;
 
 public record PasswordUpdateLinkSendRequest(
-        @Pattern(regexp = STUDENT_ID_REGEX, message = "학번 형식이 올바르지 않습니다.")
+        @Pattern(regexp = STUDENT_ID_REGEX, message = STUDENT_ID_INVALID_FORMAT)
         String studentId,
-        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        @Email(message = EMAIL_INVALID_FORMAT)
         String email
 ) {}
