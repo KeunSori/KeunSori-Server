b3ed5e54808ace388cb4e2a4f1c33d06d1aecbcd
Fluteen <161592602+Fluteen@users.noreply.github.com>
2025-07-08 18:27:44 +0900

    chore: dev profile일 때 Swagger 서버 URL 수정 (#122)

--- src/main/java/com/keunsori/keunsoriserver/global/properties/SwaggerProperties.java
+++ src/main/java/com/keunsori/keunsoriserver/global/properties/SwaggerProperties.java
@@ -3,6 +3,6 @@ package com.keunsori.keunsoriserver.global.properties;
 public class SwaggerProperties {
 
     public static final String LOCAL_SERVER_URL = "http://localhost:8080";
-    public static final String DEV_SERVER_URL = "https://keunsori-api.everdu.com";
+    public static final String DEV_SERVER_URL = "https://api.keunsori.com";
 
 }
