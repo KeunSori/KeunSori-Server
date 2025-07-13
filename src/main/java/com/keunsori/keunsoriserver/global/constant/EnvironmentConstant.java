8932b87c0a8dfa53d85213b73c00fc32aacb1bc3
kckc0608 <kckc0608@naver.com>
2025-07-14 00:12:28 +0900

    refactor: 스웨거 개발서버 url 설정 수정

--- src/main/java/com/keunsori/keunsoriserver/global/constant/EnvironmentConstant.java
+++ src/main/java/com/keunsori/keunsoriserver/global/constant/EnvironmentConstant.java
@@ -4,17 +4,18 @@ public class EnvironmentConstant {
 
     public static final String LOCAL_URL_1 = "http://localhost:5173";
     public static final String LOCAL_URL_2 = "http://localhost:5174";
-    public static final String LOCAL_URL_3 = "http://localhost:8080";
-    public static final String LOCAL_URL_4 = "https://localhost:5173";
-    public static final String LOCAL_URL_5 = "https://localhost:5174";
+    public static final String LOCAL_URL_3 = "https://localhost:5173";
+    public static final String LOCAL_URL_4 = "https://localhost:5174";
 
-    public static final String DEV_URL = "https://keun-develop.vercel.app";
-    public static final String DEV_URL2 = "https://keunsori-api.everdu.com";
-    public static final String DEV_URL3 = "https://dev.keunsori.com";
-    public static final String DEV_URL4 = "https://api.keunsori.com";
-    public static final String DEV_URL5 = "https://api.dev.keunsori.com";
+    public static final String DEV_URL_1 = "https://keun-develop.vercel.app";
+    public static final String DEV_URL_2 = "https://keunsori-api.everdu.com";
+    public static final String DEV_URL_3 = "https://dev.keunsori.com";
 
-    public static final String PROD_URL = "https://keun-sori-web.vercel.app";
-    public static final String PROD_URL2 = "https://keunsori.com";
-    public static final String PROD_URL3 = "https://www.keunsori.com";
+    public static final String PROD_URL_1 = "https://keun-sori-web.vercel.app";
+    public static final String PROD_URL_2 = "https://keunsori.com";
+    public static final String PROD_URL_3 = "https://www.keunsori.com";
+
+    public static final String LOCAL_SERVER_URL = "http://localhost:8080";
+    public static final String DEV_SERVER_URL = "https://api.dev.keunsori.com";
+    public static final String PROD_SERVER_URL = "https://api.keunsori.com";
 }
