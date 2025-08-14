371790b545c4ae875f8b6dc01102bd36fb2e6dd3
YuYeonho <yuyeonho0619@gmail.com>
2025-08-14 17:44:49 +0900

    fix: 비밀번호 링크 환경변수 적용

--- src/main/java/com/keunsori/keunsoriserver/global/constant/UrlConstant.java
+++ src/main/java/com/keunsori/keunsoriserver/global/constant/UrlConstant.java
@@ -2,7 +2,7 @@ package com.keunsori.keunsoriserver.global.constant;
 
 public class UrlConstant {
 
-    private static final String DEFAULT_DOMAIN = "https://keunsori.com/";
+    private static final String DEFAULT_DOMAIN = System.getenv("SERVER_DOMAIN");
 
     public static final String PASSWORD_CHANGE_LINK = DEFAULT_DOMAIN + "password/change";
 }
