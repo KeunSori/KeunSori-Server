743ffae1830b2955212a09f632524fffce1ea59b
YuYeonho <yuyeonho0619@gmail.com>
2025-09-05 16:00:20 +0900

    fix: 비밀번호 초기화 url 수정

--- src/main/java/com/keunsori/keunsoriserver/global/constant/UrlConstant.java
+++ src/main/java/com/keunsori/keunsoriserver/global/constant/UrlConstant.java
@@ -4,5 +4,5 @@ public class UrlConstant {
 
     private static final String DEFAULT_DOMAIN = "https://keunsori.com/";
 
-    public static final String PASSWORD_CHANGE_LINK = DEFAULT_DOMAIN + "password/change";
+    public static final String PASSWORD_CHANGE_LINK = DEFAULT_DOMAIN + "password/reset/redirect";
 }
